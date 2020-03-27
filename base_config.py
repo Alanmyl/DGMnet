b0b72ece77ca5279e3c2365e756f4024b81d5935
Alanmyl <34644622+Alanmyl@users.noreply.github.com>
2020-03-27 02:28:26 -0400

    Add files via upload

--- base_config.py
+++ base_config.py
@@ -3,16 +3,16 @@ class config:
         self._r = 0
         self._sig, self._mu = 0.25, self._r - 0.02
         self.K, self.rho, self.x0 = 1, 0.75, 1
-        self.dim, self.T = 20, 2
+        self.dim, self.T = 7, 2
         self.func_name = 'tanh'
         self.batch_size = 128
-        self.epoch_num = 30
-        self.mc_num = 500
-        self.delta = 1e-3
-        self.unit_num = 16
+        self.epoch_num = 20000
+        self.mc_num = 512
+        self.delta = 1e-4
+        self.unit_num = 32
         self.layer_num = 3
-        self.proportion1 = 0.5
-        self.proportion2 = 0.3
+        self.xmax = 5
+        self.filepath = '/home/alanmei/Alan/Columbia University/Research/'
 
 Config = config()
     
--- custom_model.py
+++ custom_model.py
@@ -4,37 +4,57 @@
 from base_config import Config
 import tensorflow as tf
 import tensorflow_probability as tfp
-
-class lstm_layer(tf.keras.layers.Layer):
+from scipy.sparse import diags
+import numpy as np
+import scipy.stats as stats
+import logging, os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
+logging.getLogger('tensorflow').setLevel(logging.FATAL)
+tf.random.set_seed(1234)
+tf.autograph.set_verbosity(1)
+class DGM_layer(tf.keras.layers.Layer):
 
     def __init__(self, units=Config.unit_num):
-        super(lstm_layer, self).__init__()
+        super(DGM_layer, self).__init__(trainable=True, name='DGM', dtype=tf.float32)
         self.units = units
+        self.matrix = tf.linalg.cholesky(Config.delta * (tf.ones(shape=(Config.dim, Config.dim)) * Config.rho+ tf.eye(Config.dim) * (1-Config.rho)))
     
     def build(self, input_shape):
-        self.w1 = self.add_weight(shape=(input_shape[-1], self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.b1 = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True)
-        self.uzl = self.add_weight(shape=(input_shape[-1], self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.wzl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.bzl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True)
-        self.ugl = self.add_weight(shape=(input_shape[-1], self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.wgl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.bgl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True)
-        self.url = self.add_weight(shape=(input_shape[-1], self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.wrl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.brl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True)
-        self.uhl = self.add_weight(shape=(input_shape[-1], self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.whl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True)
-        self.bhl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True)
-        self.w = self.add_weight(shape=(self.units, 1), initializer=tf.random_uniform_initializer, trainable=True)
-        self.b = self.add_weight(shape=(1,), initializer=tf.random_uniform_initializer, trainable=True)
-
+        ops_size = input_shape[-1]
+        self.w1 = self.add_weight(shape=(ops_size, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='w1')
+        self.b1 = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True, name='b1')
+        self.uzl = self.add_weight(shape=(ops_size, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='uzl')
+        self.wzl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='wzl')
+        self.bzl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True, name='bzl')
+        self.ugl = self.add_weight(shape=(ops_size, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='ugl')
+        self.wgl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='wgl')
+        self.bgl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True, name='bgl')
+        self.url = self.add_weight(shape=(ops_size, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='url')
+        self.wrl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='wrl')
+        self.brl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True, name='brl')
+        self.uhl = self.add_weight(shape=(ops_size, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='uhl')
+        self.whl = self.add_weight(shape=(self.units, self.units), initializer=tf.random_uniform_initializer, trainable=True, name='whl')
+        self.bhl = self.add_weight(shape=(self.units,), initializer=tf.random_uniform_initializer, trainable=True, name='bhl')
+        self.w = self.add_weight(shape=(self.units, 1), initializer=tf.random_uniform_initializer, trainable=True, name='w')
+        self.b = self.add_weight(shape=(1,), initializer=tf.random_uniform_initializer, trainable=True, name='b') 
+    
     def act_func(self, x, name=Config.func_name):
         names = ['tanh', 'relu', 'sigmoid']
         assert name in names, "Undefined activation function"
-        return tf.switch_case(tf.constant(names.index(name)), branch_fns={0: lambda: tf.keras.activations.tanh(x), 1: lambda: tf.keras.activations.relu(x), 2: lambda: tf.keras.activations.sigmoid(x)})
+        if name == 'tanh':
+            return tf.keras.activations.tanh(x)
+        elif name == 'relu':
+            return tf.keras.activations.relu(x)
+        else:
+            return tf.keras.activations.sigmoid(x)
 
-    def call(self, inputs):
+    def sigma(self, x):
+        return Config._sig * x
+    
+    def mu(self, x):
+        return Config._mu * x
+
+    def value(self, inputs):
         s = self.act_func(tf.matmul(inputs, self.w1) + self.b1)
         for i in range(Config.layer_num):
             z = self.act_func(tf.matmul(inputs, self.uzl) + tf.matmul(s, self.wzl)  + self.bzl)
@@ -43,121 +63,119 @@ class lstm_layer(tf.keras.layers.Layer):
             h = self.act_func(tf.matmul(inputs, self.uhl)  + tf.matmul(tf.math.multiply(s,r), self.whl) + self.bhl)
             s = tf.math.multiply(1.0 - g, h) + tf.math.multiply(z, s)
         return tf.matmul(s, self.w) + self.b
-    '''
-    def dynamic_pred(self, inputs):
-        print(inputs)
+    
+    def boundary(self, x):
+        return tf.nn.relu(tf.math.pow(tf.math.reduce_prod(x, axis=1), 1 / tf.cast(tf.shape(x)[-1], tf.float32)))
+
+    def call(self, inputs):
+        input1, input2 = tf.split(inputs,2, 0)
         with tf.GradientTape() as g1:
-            g1.watch(inputs)
-            ftx = self.value(inputs)
-        fprime = g1.gradient(ftx, inputs)
-        term1_1 = tf.reduce_sum(tf.multiply(tf.concat([self.mu(inputs[:,:-1]), tf.ones(shape=(tf.shape(inputs)[0], 1))], axis=1), fprime), axis=1)
-        matrix = Config.delta * (tf.ones(shape=(inputs.shape[1] - 1, inputs.shape[1] - 1)) * 0.75+ tf.eye(inputs.shape[1] - 1) * 0.25)
-        cov = lambda x: tfp.distributions.MultivariateNormalTriL(loc=x, scale_tril=tf.linalg.cholesky(matrix)).sample(sample_shape=(Config.mc_num))*tf.broadcast_to(self.sigma(x),shape=(Config.mc_num, inputs.shape[-1]-1))
-        violation = tf.concat([tf.vectorized_map(cov, inputs[:,:-1]), tf.zeros(shape=(inputs.shape[0], Config.mc_num,1))],axis=2)
-        xplus = tf.expand_dims(inputs,axis=1) + violation
+            g1.watch(input1)
+            ftx = self.value(input1)
+        fprime = g1.gradient(ftx, input1)
+        term1_1 = tf.reduce_sum(tf.multiply(tf.concat([self.mu(input1[:,:-1]), tf.ones(shape=(tf.shape(input1)[0], 1))], axis=1), fprime), axis=1)
+        xdiff = tfp.distributions.MultivariateNormalTriL(loc=input1[:,:-1], scale_tril=self.matrix).sample(sample_shape=(Config.mc_num))*tf.broadcast_to(self.sigma(input1[:,:-1]),shape=(Config.mc_num,input1.shape[0], input1.shape[1]-1))
+        violation = tf.concat([xdiff, tf.zeros(shape=(Config.mc_num, input1.shape[0], 1))], axis=2)
+        xplus = tf.expand_dims(input1,axis=0) + violation
         with tf.GradientTape() as g1_1:
             g1_1.watch(xplus)
             ftx_plus = self.value(xplus)
         fprime_plus = g1_1.gradient(ftx_plus, xplus)
-        term1_2 = tf.reduce_sum(tf.reduce_mean(tf.multiply((fprime_plus - tf.expand_dims(fprime, axis=1)) / Config.delta, violation), axis=2), axis=1)
-        return term1_1 + term1_2 * 0.5- Config._r * tf.squeeze(self.value(inputs))
-    
-    def constraint_pred(self, inputs):
-        return tf.nn.relu(self.boundary(inputs[:,:-1]) - tf.squeeze(self.value(inputs)))
-
-    def boundary_pred(self, inputs):
-        return tf.squeeze(self.value(inputs))-self.boundary(inputs[:,:-1])
-    
-    def call(self, inputs):
-        res = []
-        for i in range(3):
-            print(tf.equal(tf.cast(inputs[:, -1], tf.int32), i))
-            print(inputs)
-            data = tf.boolean_mask(inputs, tf.equal(tf.cast(inputs[:, -1], tf.int32), tf.constant(i)))
-            print(data)
-            res.append(tf.switch_case(tf.cast(data[0,-1], tf.int32), branch_fns={0: lambda: self.dynamic_pred(data[:, :-1]), 1: lambda: self.constraint_pred(data[:, :-1]), 2: lambda: self.boundary_pred(data[:, :-1])}))
-        return tf.concat(res,axis=0)'''
-        
-class dynamic_layer(tf.keras.layers.Layer):
-
-    def __init__(self, units=Config.unit_num):
-        super(dynamic_layer, self).__init__()
-        self.units = units
-
-    def sigma(self, x):
-        return Config._sig * x
-    
-    def mu(self, x):
-        return Config._mu * x
-
-    def call(self, inputs, layer):
-        with tf.GradientTape() as g1:
-            g1.watch(inputs)
-            ftx = layer(inputs)
-        fprime = g1.gradient(ftx, inputs)
-        term1_1 = tf.reduce_sum(tf.multiply(tf.concat([self.mu(inputs[:,:-1]), tf.ones(shape=(tf.shape(inputs)[0], 1))], axis=1), fprime), axis=1)
-        matrix = Config.delta * (tf.ones(shape=(inputs.shape[1] - 1, inputs.shape[1] - 1)) * 0.75+ tf.eye(inputs.shape[1] - 1) * 0.25)
-        cov = lambda x: tfp.distributions.MultivariateNormalTriL(loc=x, scale_tril=tf.linalg.cholesky(matrix)).sample(sample_shape=(Config.mc_num))*tf.broadcast_to(self.sigma(x),shape=(Config.mc_num, inputs.shape[-1]-1))
-        violation = tf.concat([tf.vectorized_map(cov, inputs[:,:-1]), tf.zeros(shape=(inputs.shape[0], Config.mc_num,1))],axis=2)
-        xplus = tf.expand_dims(inputs,axis=1) + violation
-        with tf.GradientTape() as g1_1:
-            g1_1.watch(xplus)
-            ftx_plus = layer(xplus)
-        fprime_plus = g1_1.gradient(ftx_plus, xplus)
-        term1_2 = tf.reduce_sum(tf.reduce_mean(tf.multiply((fprime_plus - tf.expand_dims(fprime, axis=1)) / Config.delta, violation), axis=2), axis=1)
-        return term1_1 + term1_2 * 0.5- Config._r * tf.squeeze(layer(inputs))
-    
-class boundary_layer(tf.keras.layers.Layer):
-
-    def __init__(self, units=Config.unit_num):
-        super(boundary_layer, self).__init__()
-        self.units = units
-
-    def boundary(self, x):
-        return tf.nn.relu(tf.math.pow(tf.math.reduce_prod(x, axis=1), tf.cast(tf.shape(x)[-1], tf.float32)))
-        
-    def call(self, inputs, layer):
-        return tf.squeeze(layer(inputs)) - self.boundary(inputs[:,:-1])
-        
-class constraint_layer(tf.keras.layers.Layer):
-
-    def __init__(self, units=Config.unit_num):
-        super(constraint_layer, self).__init__()
-        self.units = units
-
-    def boundary(self, x):
-        return tf.nn.relu(tf.math.pow(tf.math.reduce_prod(x, axis=1), tf.cast(tf.shape(x)[-1], tf.float32)))
-    
-    def call(self, inputs, layer):
-        return tf.nn.relu(self.boundary(inputs[:,:-1]) - tf.squeeze(layer(inputs)))
-
-        
-
-def custom_loss(y_true, y_pred):
-    return tf.reduce_sum(tf.math.square(y_pred))
+        term1_2 = tf.reduce_sum(tf.reduce_mean(tf.multiply((fprime_plus - tf.expand_dims(fprime, axis=0)) / Config.delta, violation), axis=0), axis=1)
+        term2 = tf.squeeze(self.value(input2)) - self.boundary(input2[:,:-1])
+        #term3 = tf.nn.relu(self.boundary(input3[:,:-1]) - tf.squeeze(self.value(input3)))
+        #test = tf.concat([term1_1 + term1_2 * 0.5- Config._r * tf.squeeze(self.value(input1)), term2, term3], axis=0)
+        test = tf.concat([term1_1 + term1_2 * 0.5- Config._r * tf.squeeze(self.value(input1)), term2], axis=0)
+        return test
 
 
-num1, num2 = int(Config.batch_size * Config.proportion1 / Config.unit_num)*Config.unit_num, int(Config.batch_size * Config.proportion2 / Config.unit_num)*Config.unit_num
-num3 = Config.batch_size - num1 - num2
-num1, num2, num3 = 128, 128, 128
-input1 = tf.keras.Input(shape=(Config.dim + 1,), batch_size=num1)
-input2 = tf.keras.Input(shape=(Config.dim + 1,), batch_size=num2)
-input3 = tf.keras.Input(shape=(Config.dim + 1,), batch_size=num2)
-layer = lstm_layer()
-output1 = dynamic_layer()(input1,layer)
-output2 = boundary_layer()(input2, layer)
-output3 = constraint_layer()(input3, layer)
-model = tf.keras.Model(inputs=[input1, input2, input3], outputs=[output1, output2, output3])
-opt = tf.keras.optimizers.Adam(learning_rate=1e-4)
+train = tf.keras.Input(shape=(Config.dim + 1,), batch_size=Config.batch_size*2)
+model = tf.keras.Model(inputs=train, outputs=DGM_layer()(train))
+opt = tf.keras.optimizers.Adam(learning_rate=tf.keras.optimizers.schedules.PiecewiseConstantDecay([2000,5000,7500,10000],[1e-4, 4e-5, 1.5e-5,6e-6,2e-6]))
 
+class weighted_loss(tf.keras.losses.Loss):
+
+    def call(self, y_true, y_pred):
+        loss1, loss2 = tf.split(tf.math.square(y_pred - y_true), 2, axis=0)
+        base = tf.reduce_sum(loss1)
+        loss2 = loss2 / tf.reduce_sum(loss2) * base
+        return tf.reduce_mean(loss1)/2.0 + tf.reduce_mean(loss2)/2.0
+
+def generate_data(tfixed=False):
+    part1 = tf.math.exp(tf.random.uniform(shape=(Config.batch_size, Config.dim), minval=[-Config.xmax for i in range(Config.dim)], maxval=[Config.xmax for i in range(Config.dim)]))
+    if tfixed == True:
+        part2 = tf.ones(shape=(Config.batch_size, 1)) * Config.T
+    else:
+        part2 = tf.random.uniform(shape=(Config.batch_size, 1), maxval=[Config.T])
+    return tf.concat([part1, part2], axis=1)
+
+import datetime as dt
+loss_hist = []
+start = dt.datetime.now()
+gen_time = dt.datetime.now()-dt.datetime.now()
 for i in range(Config.epoch_num):
-    data1 = tf.random.uniform(shape=(num1, Config.dim + 1), maxval=[10 for i in range(Config.dim)] + [Config.T])
-    data2 = tf.random.uniform(shape=(num2, Config.dim + 1), maxval=[10 for i in range(Config.dim)] + [Config.T])
-    data3 = tf.random.uniform(shape=(num3, Config.dim + 1), minval=[0 for i in range(Config.dim)] + [Config.T], maxval=[10 for i in range(Config.dim)] + [Config.T])
+    mid = dt.datetime.now()
+    data1 = generate_data()
+    data2 = generate_data()
+    #data3 = generate_data(True)
+    data = tf.concat([data1, data2], axis=0)
+    
     with tf.GradientTape() as tape:
-        loss = custom_loss(0.0, model([data1, data2, data3], training=True))
+        loss = tf.keras.losses.MeanSquaredError()(0.0, model(data))
+    gen_time = gen_time+dt.datetime.now()-mid
     grads = tape.gradient(loss, model.trainable_weights)
+    
+    loss_hist.append(loss.numpy())
+    
     opt.apply_gradients(zip(grads, model.trainable_weights))
+    if i%10 == 0:
+        print('{} epochs spend {} with loss {:.3f}'.format(i, dt.datetime.now() - start, loss))
+        print('generate {} epochs spend {}'.format(i, gen_time))
+    if (i+1) % 1000 == 0:
+        model.save(filepath=Config.filepath + "log/model" + str(i))
+    
 
+def bs_option(opt_type, r, sig, T, K, s0):
+    '''function to calculate options price with Black-Scholes formula.
 
-# assume (t,x) are uniformly distributed
+    Args:
+        opt_type: `call` or `put`.
+        r: risk-free rate under the risk-neutral measure.
+        sig: volatility.
+        T: time to maturity.
+        K: strike price.
+        s0:initial price.
+
+    Returns:
+        A value or a sequence of values with datetime index.
+    '''
+    assert opt_type in ['call', 'put'], 'Wrong argument '+opt_type
+    d1 = (np.log(s0 / K) + (r + sig ** 2 / 2) * T) / sig / T ** 0.5
+    d2 = d1 - sig * T ** 0.5
+    if opt_type == 'put':
+        return stats.norm.cdf(-d2) * K * np.exp(-r * T) - stats.norm.cdf(-d1) * s0
+    else:
+        return stats.norm.cdf(d1) * s0 - stats.norm.cdf(d2) * K * np.exp(-r * T)
+
+sample = tf.concat([generate_data(),generate_data()],axis=0)
+option_mu = Config._mu-Config._sig ** 2/2
+option_sig = Config._sig/np.sqrt(Config.dim)
+eu_opt = bs_option('put', option_mu, option_sig, 0.01, Config.K, np.product(sample[:-1],axis=1))
+res = model.get_layer(name='DGM').value(sample[:Config.batch_size])
+print(model.predict(sample))
+import matplotlib.pyplot as plt
+plt.plot(loss_hist)
+plt.savefig(Config.filepath+'loss.png')
+print(loss_hist)
+import pandas as pd
+pd.Series(loss_hist).to_csv(Config.filepath+'loss.csv')
+
+'''
+# finite difference method for pricing American options
+N, Nt = int(1e5), int(2e3)
+h, tau = Config.xmax / N, Config.T / Nt
+gamma1 = (Config._mu - Config._sig ** 2 / 2) / 2 * tau / h  - Config._sig ** 2 * tau / h ** 2 / 2
+gamma2 = 1 - Config._sig ** 2 * tau / h ** 2
+gamma3 = -(Config._mu - Config._sig ** 2 / 2) / 2 * tau / h  - Config._sig ** 2 * tau / h ** 2 / 2
+C = diags([[gamma1],[gamma2],[gamma3]], [-1, 0, 1], shape=(N+1, N+1))
+'''
